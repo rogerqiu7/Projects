ad43ffbed73bc58fc937504f7a8b81dcbbb52580
Roger Qiu <84350865+rogerqiu7@users.noreply.github.com>
2022-04-23 18:46:58 -0500

    update

--- Blockchain/run.py
+++ Blockchain/run.py
@@ -7,7 +7,6 @@ from blockchain import Blockchain
 block_one_transactions = {"sender":"Roger", "receiver": "John", "amount":"50"}
 block_two_transactions = {"sender": "John", "receiver":"Bob", "amount":"30"}
 block_three_transactions = {"sender":"Bob", "receiver":"Roger", "amount":"40"}
-block_four_transactions = {"sender":"Tucker","receiver":"Simba","amount":"0.2"}
 
 #create a fake transaction
 fake_transactions = {"sender": "Roger", "receiver":"Bob", "amount":"100"}
@@ -19,14 +18,13 @@ local_blockchain = Blockchain()
 local_blockchain.add_block(block_one_transactions)
 local_blockchain.add_block(block_two_transactions)
 local_blockchain.add_block(block_three_transactions)
-local_blockchain.add_block(block_four_transactions)
 
 
 # print out all the blocks
 local_blockchain.print_blocks()
 # prints out the genesis block as Block 0, the timestamp, its empty transactions, current hash and previous hash of 0
 
-#local_blockchain.chain[2].transactions = fake_transactions
-#local_blockchain.validate_chain()
+local_blockchain.chain[2].transactions = fake_transactions
+local_blockchain.validate_chain()
 # print out that current hash does not equal generated hash and validated as False
 # current hash will not equal the generated hash because existing block already has a hash
